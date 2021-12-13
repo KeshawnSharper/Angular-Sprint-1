8fc87274cc8edd97c1bdb5e06b109b62e93d460a
keshawn0998 <keshawn@contenda.co>
2021-12-13 12:06:26 -0500

    Got the data from the console

--- angularSprint1/src/app/app.component.ts
+++ angularSprint1/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient){}
+
   title = 'angularSprint1';
+  ngOnInit() {
+    this.http.get<any>('https://cat-fact.herokuapp.com/facts').subscribe(data => {
+        console.log(data);
+    })        
+  }
 }
